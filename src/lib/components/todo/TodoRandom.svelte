6ca80e105c88bbb8a96029febe8c03c74dac42b2
AnthonySmith8686 <anthonysmith8686@gmail.com>
2023-05-16 23:30:43 +0100

    update

---
+++ src/lib/components/todo/TodoRandom.svelte
@@ -0,0 +1,9 @@
+<script>
+import { todos } from "../../store/TodoStore";
+</script>
+
+<section class="todo-list">
+ {#each $todos as todo}
+  <p>{todo.text} - {todo.complete}</p>
+ {/each}
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<div class="container mx-auto">
- <h1 class="text-4xl font-bold underline">Welcome to SvelteKit</h1>
- <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AddTodoSection from '../lib/components/todo/AddTodoSection.svelte';
+	import TodoSection from '../lib/components/todo/TodoSection.svelte';
+	import ProgressSection from '../lib/components/progress/ProgressSection.svelte';
+ import TodoRandom from '../lib/components/todo/TodoRandom.svelte';
+
+</script>
+
+<div class="min-h-screen bg-olive-one p-0 selection:bg-green-two md:py-24 md:px-8">
+	<main
+		class="mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 py-10 outline-none md:min-h-max md:max-w-[60rem] md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-cream-four"
+	>
+		<h1 class="text-2xl font-bold text-gray-three">Dashboard</h1>
+		<div class="mt-10">
+			<AddTodoSection />
+			<TodoSection />
+			<ProgressSection />
+			<TodoRandom />
+		</div>
+	</main>
 </div>
